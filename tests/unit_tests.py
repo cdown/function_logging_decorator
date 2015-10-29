9e5e8e56629943c499c3a4e40369c6527d371953
Chris Down <chris@chrisdown.name>
2015-10-29 17:53:23 +0000

    Test with hypothesis

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,9 +1,18 @@
 from logdecorator import log_when_called
 import testfixtures
 import logging
+from hypothesis import given
+import hypothesis.strategies as st
 
 LOGGER_NAME = 'hunter2'
 log = logging.getLogger(LOGGER_NAME)
+
+
+ALL_SORTS_ITEMS = (
+    st.integers(), st.text(), st.floats(), st.booleans(), st.complex_numbers(),
+    st.tuples(), st.fractions(), st.decimals(),
+)
+ALL_SORTS = st.tuples(st.recursive(st.one_of(*ALL_SORTS_ITEMS), st.lists))
 
 
 @log_when_called(log)
@@ -11,13 +20,14 @@ def example_func(*args, **kwargs):
     pass
 
 
-def test_basic_log():
+@given(ALL_SORTS)
+def test_basic_log(args):
     with testfixtures.LogCapture() as logs:
-        example_func('a', 1, ['x'])
+        example_func(args)
 
     logs.check(
         (
             LOGGER_NAME, 'DEBUG',
-            "Calling example_func('a', 1, ['x'])",
+            "Calling example_func(%r)" % (args,),
         )
     )
