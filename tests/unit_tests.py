ebf743c00484fac35d443aa0655862c39e84a2d5
Chris Down <chris@chrisdown.name>
2015-10-29 15:37:39 +0000

    Add basic test

---
+++ tests/unit_tests.py
@@ -0,0 +1,23 @@
+from logging_decorator import log_when_called
+import testfixtures
+import logging
+
+LOGGER_NAME = 'hunter2'
+log = logging.getLogger(LOGGER_NAME)
+
+
+@log_when_called(log)
+def example_func(*args, **kwargs):
+    pass
+
+
+def test_basic_log():
+    with testfixtures.LogCapture() as logs:
+        example_func('a', 1, ['x'])
+
+    logs.check(
+        (
+            LOGGER_NAME, 'DEBUG',
+            "Calling example_func('a', 1, ['x'])",
+        )
+    )
